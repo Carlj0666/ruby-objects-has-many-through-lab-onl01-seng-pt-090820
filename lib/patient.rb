c771730d55bddb58cee495ba4ced990352f01491
Carl J <carlj1881@yahoo.com>
2020-10-08 00:06:38 +0000

    Automatically backed up by Learn

---
+++ lib/patient.rb
@@ -0,0 +1,37 @@
+require "pry"
+
+
+class Patient
+  
+  attr_accessor 
+  
+  @@all = []
+  
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  
+  def self.all
+    @@all
+  end
+  
+  
+  def new_appointment(date, doctor)
+    #binding.pry
+    return Appointment.new(date, self, doctor)
+  end
+    
+    
+  def appointments
+    Appointment.all.map do |appointment| # could use select
+      if appointment.patient = self
+        appointment.patient
+      end
+    end
+  end
+  
+  
+end
